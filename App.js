6769c98f01d8d385afb2e3fa72e02f4a7f1825b0
JaakkoSai <jsailynoja@gmail.com>
2024-04-12 11:03:03 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomePage from "./pages/HomePage";
+import MapPage from "./pages/MapPage";
+import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomePage} />
+        <Tab.Screen name="Map" component={MapPage} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyCuMiE-RxfU7ztepPRZ6dWbuVd5QZnUyEA",
+  authDomain: "mapapp-9f313.firebaseapp.com",
+  projectId: "mapapp-9f313",
+  storageBucket: "mapapp-9f313.appspot.com",
+  messagingSenderId: "834274424954",
+  appId: "1:834274424954:web:b279d6a385dd875b9aa3c3",
+  measurementId: "G-9XK00WPPZK",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ pages/MapPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+
+export default function MapPage() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
